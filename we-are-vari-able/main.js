0cc55fcf80ea4845484636fdffd4bcfd23d3f4b6
Jesse <163429092+fishrifle@users.noreply.github.com>
2024-06-19 18:29:58 -0500

    checking work

---
+++ we-are-vari-able/main.js
@@ -0,0 +1,84 @@
+/******************
+ * YOUR CODE BELOW *
+ ******************/
+let myFirstName = "Jesse"
+let myLastName = "Davis"
+let myFavoriteCeleb = "Jim Carey"
+let mySentence = "myFirstName + myLastName + myFavoriteCeleb"
+let myLuckyNum = 13
+let myAge = 38
+let myHeight = 69
+let myFavoriteYear = 2000
+let mySum = 5 + 2317 + 36
+console.log(mySum)
+let myProduct = 232 * 43 / 2 + 4
+console.log(myProduct)
+let myHeightAndAge = myAge + myHeight
+console.log(myHeightAndAge)
+
+
+/*********************************************************************************************************************
+
+ * CODE BELOW HERE IS FOR INTERNAL USE ONLY! IT ENSURES THAT THE TESTING *
+ * CAN READ AND EVALUATE YOUR CODE. *
+ * DON'T CHANGE ANYTHING BELOW HERE, PLEASE! *
+
+ *********************************************************************************************************************/
+
+if (typeof myFirstName === "undefined") {
+  myFirstName = undefined;
+}
+
+if (typeof myLastName === "undefined") {
+  myLastName = undefined;
+}
+
+if (typeof myFavoriteCeleb === "undefined") {
+  myFavoriteCeleb = undefined;
+}
+
+if (typeof mySentence === "undefined") {
+  mySentence = undefined;
+}
+
+if (typeof myLuckyNum === "undefined") {
+  myLuckyNum = undefined;
+}
+
+if (typeof myAge === "undefined") {
+  myMentalAge = undefined;
+}
+
+if (typeof myHeight === "undefined") {
+  myHeight = undefined;
+}
+
+if (typeof myFavoriteYear === "undefined") {
+  myFavoriteYear = undefined;
+}
+
+if (typeof mySum === "undefined") {
+  mySum = undefined;
+}
+
+if (typeof myProduct === "undefined") {
+  myProduct = undefined;
+}
+
+if (typeof myHeightAndAge === "undefined") {
+  myHeightAndAge = undefined;
+}
+
+module.exports = {
+  myFirstName,
+  myLastName,
+  myFavoriteCeleb,
+  mySentence,
+  myLuckyNum,
+  myAge,
+  myHeight,
+  myFavoriteYear,
+  mySum,
+  myProduct,
+  myHeightAndAge,
+};
